9bb3d2efb282e3b8468aa55ac21394f98a11a6e3
gemmaliang06 <gtl2024@nyu.edu>
2025-03-15 18:02:08 -0400

    js

--- Directory/Midterm/Carousel.js
+++ Directory/Midterm/Carousel.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 const nextButton =  document.querySelector('.carousel__button--right');
@@ -26,7 +27,7 @@ const updateDots = (currentDot, targetDot) => {
 }
 
 const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-    if (tagetIndex === 0) {
+    if (targetIndex === 0) {
         prevButton.classList.add('is-hidden');
         nextButton.classList.remove('is-hidden');
     } else if (targetIndex === slides.length - 1) {
@@ -42,7 +43,7 @@ const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
 
 // where I click left, move sliders to the left
 prevButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.currentSlide');
+    const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
     const currentDot = dotsNav.querySelector('.current-slide');
     const prevDot = currentDot.previousElementSibling;
@@ -50,22 +51,22 @@ prevButton.addEventListener('click', e => {
     
     moveToSlide(track, currentSlide, prevSlide);
     updateDots(currentDot, prevDot);
-    hideShowArrows = (slides, prevButton, nextButton, prevIndex);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
 
 });
 
 
 // where I click right, move sliders to the right
 nextButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.currentSlide');
+    const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
     const currentDot = dotsNav.querySelector('.current-slide');
     const nextDot = currentDot.nextElementSibling;
     const nextIndex = slides.findIndex(slide => slide === nextSlide);
     
     moveToSlide(track, currentSlide, nextSlide);
-    updateDots(currentDot, targetDot);
-    hideShowArrows = (slides, prevButton, nextButton, nextIndex);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
 
 
@@ -77,14 +78,13 @@ dotsNav.addEventListener('click', e => {
 
     if(!targetDot) return;
 
-    const currentSlide = track.querySelector('.currentSlide');
+    const currentSlide = track.querySelector('.current-slide');
     const currentDot = dotsNav.querySelector('.current-slide');
     const targetIndex = dots.findIndex(dot => dot === targetDot);
     const targetSlide = slides[targetIndex];
    
     moveToSlide(track, currentSlide, targetSlide);
     updateDots(currentDot, targetDot);
-    hideShowArrows = (slides, prevButton, nextButton, targetIndex);
-
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
 });
-
+});
