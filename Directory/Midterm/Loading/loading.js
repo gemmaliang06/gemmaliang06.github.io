8ed4108db55ef151fd742db60bea82a3d7fb5487
gemmaliang06 <gtl2024@nyu.edu>
2025-04-11 15:10:28 -0400

    final

---
+++ Directory/Midterm/Loading/loading.js
@@ -0,0 +1,10 @@
+// const tl = gsap.timeline({});
+
+// console.log("Starting animation...");
+// console.log(gsap.timeline);
+
+// gsap.to('.gallery_box_outer', {
+//     duration: 5,
+//     rotateY: 180,
+//     ease: "none",
+//   });
